4abe30b84ddb03e876428180bb6a640844a2f7a1
Andrew Bourgeois <andrew@olympicmedia.com>
2020-04-22 18:51:47 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -4,7 +4,7 @@ def second_challenge
       :partriarch => {}
       :matriarch => {}
       :hero => {}
-      :hero_friends => {}
+      :hero_friends => []
       }, 
     :capulet => {
       #your key/value pairs here!
